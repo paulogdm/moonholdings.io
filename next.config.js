9a93df3583afcb5468fc7616537d34196a3cdb8c
Leon Gaban <leongaban@gmail.com>
2019-01-21 10:54:19 -0600

    Fixed now.json file

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
-// next.config.js
-const withSass = require('@zeit/next-sass')
-module.exports = withSass()
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {}
+    : !process.env.NOW_REGION
+      ? require('next/constants')
+      : require('next-server/constants');
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
+  }
+
+  // ✅ Put the require call here.
+  const withCSS = require('@zeit/next-sass');
+
+  return withCSS();
+};
